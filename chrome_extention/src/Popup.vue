913a9c6539b4c6775596b5e71f74b7ad7ff0fd3a
ryan <wyrms01@outlook.com>
2025-11-02 12:41:32 +0800

    vue build for crx

---
+++ chrome_extention/src/Popup.vue
@@ -0,0 +1,11 @@
+<template>
+  <div style="width: 200px; height: 120px; display: flex; justify-content: center; align-items: center; background: #282c34; color: white;">
+    <h2>Hello World!</h2>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
---
+++ chrome_extention/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'popup.html')
+      },
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    },
+    outDir: 'dist'
+  }
+})
